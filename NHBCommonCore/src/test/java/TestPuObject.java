7d6cff4262394118cfe6124fa56575979654c12c
bachden <hoangbach.bk@gmail.com>
2017-05-08 20:59:32 +0700

    ...

--- NHBCommonCore/src/test/java/TestPuObject.java
+++ NHBCommonCore/src/test/java/TestPuObject.java
@@ -8,13 +8,20 @@ public class TestPuObject {
 
 	public static void main(String[] args) throws ParseException, IOException {
 
+		System.out.println("Test string");
 		PuObject value = new PuObject();
 		value.set("name", "bachden");
-		value.set("bytes", new byte[] { 1, 2, 3 });
-		value.set("age", 28);
-
+		// value.set("age", 28);
 		byte[] bytes = value.toBytes();
+		System.out.println("--> " + PuObject.fromObject(bytes));
 
+		System.out.println("Test byte array");
+		value = new PuObject();
+		value.set("bytes",
+				new byte[] { 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2,
+						3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2,
+						3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4 });
+		bytes = value.toBytes();
 		System.out.println("--> " + PuObject.fromObject(bytes));
 	}
 }
