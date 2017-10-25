cd9b9ac57bc07b9a3fb0f2cf777bf8597116b7ac
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2017-10-25 17:40:06 +0700

    Added support for PoolConfig in ZMQSocketRegistry

--- NHBCommonMessaging/src/main/java/com/nhb/messaging/zmq/pool/ZMQSocketPool.java
+++ NHBCommonMessaging/src/main/java/com/nhb/messaging/zmq/pool/ZMQSocketPool.java
@@ -1,6 +1,7 @@
 package com.nhb.messaging.zmq.pool;
 
 import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 import com.nhb.messaging.zmq.ZMQSocket;
 import com.nhb.messaging.zmq.ZMQSocketRegistry;
@@ -10,5 +11,10 @@ public class ZMQSocketPool extends GenericObjectPool<ZMQSocket> {
 
 	public ZMQSocketPool(ZMQSocketRegistry socketRegistry, String address, ZMQSocketType socketType) {
 		super(new ZMQSocketPooledObjectFactory(socketRegistry, address, socketType));
+	}
+
+	public ZMQSocketPool(ZMQSocketRegistry socketRegistry, String address, ZMQSocketType socketType,
+			GenericObjectPoolConfig config) {
+		super(new ZMQSocketPooledObjectFactory(socketRegistry, address, socketType), config);
 	}
 }
