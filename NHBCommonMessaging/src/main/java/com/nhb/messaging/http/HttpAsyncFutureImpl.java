5247009a6ef0b5a356a4c6c75a78d7060df6bdc1
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2019-05-06 00:06:21 +0700

    Fix bug infinity waiting when call cancel in BaseRPCFuture, upgrade version to 1.5.19

--- NHBCommonMessaging/src/main/java/com/nhb/messaging/http/HttpAsyncFutureImpl.java
+++ NHBCommonMessaging/src/main/java/com/nhb/messaging/http/HttpAsyncFutureImpl.java
@@ -22,70 +22,66 @@ import lombok.Getter;
 
 class HttpAsyncFutureImpl extends BaseRPCFuture<HttpResponse> implements HttpAsyncFuture {
 
-    @Getter
-    private final HttpUriRequest request;
+	@Getter
+	private final HttpUriRequest request;
 
-    @Getter
-    private final HttpContext context;
+	@Getter
+	private final HttpContext context;
 
-    @Getter(AccessLevel.PACKAGE)
-    private final FutureCallback<HttpResponse> futureCallback = new FutureCallback<HttpResponse>() {
+	@Getter(AccessLevel.PACKAGE)
+	private final FutureCallback<HttpResponse> futureCallback = new FutureCallback<HttpResponse>() {
 
-        @Override
-        public void failed(Exception ex) {
-            HttpAsyncFutureImpl.this.failed(ex);
-        }
+		@Override
+		public void failed(Exception ex) {
+			HttpAsyncFutureImpl.this.setFailedAndDone(ex);
+		}
 
-        @Override
-        public void completed(HttpResponse result) {
-            HttpAsyncFutureImpl.this.setAndDone(result);
-        }
+		@Override
+		public void completed(HttpResponse result) {
+			HttpAsyncFutureImpl.this.setAndDone(result);
+		}
 
-        @Override
-        public void cancelled() {
-            // do nothing.
-        }
-    };
+		@Override
+		public void cancelled() {
+			// do nothing.
+		}
+	};
 
-    HttpAsyncFutureImpl(HttpUriRequest request, HttpContext context) {
-        this.request = request;
-        this.context = context;
-    }
+	HttpAsyncFutureImpl(HttpUriRequest request, HttpContext context) {
+		this.request = request;
+		this.context = context;
+	}
 
-    private void failed(Exception exception) {
-        this.setFailedAndDone(exception);
-    }
+	private PuElement handleResponse(HttpResponse response) {
+		PuElement result = null;
+		if (response != null) {
+			try {
+				String responseText = EntityUtils.toString(response.getEntity());
+				if (responseText != null) {
+					responseText = responseText.trim();
+					try {
+						if (responseText.startsWith("[")) {
+							result = PuArrayList.fromJSON(responseText);
+						} else if (responseText.startsWith("{")) {
+							result = PuObject.fromJSON(responseText);
+						} else if (responseText.startsWith("<")) {
+							result = PuXmlHelper.parseXml(responseText);
+						} else {
+							result = new PuValue(responseText, PuDataType.STRING);
+						}
+					} catch (Exception ex) {
+						result = new PuValue(responseText, PuDataType.STRING);
+					}
+				}
+			} catch (IOException e) {
+				throw new RuntimeException("Error while consuming response entity", e);
+			}
+		}
+		return result;
+	}
 
-    private PuElement handleResponse(HttpResponse response) {
-        PuElement result = null;
-        if (response != null) {
-            try {
-                String responseText = EntityUtils.toString(response.getEntity());
-                if (responseText != null) {
-                    responseText = responseText.trim();
-                    try {
-                        if (responseText.startsWith("[")) {
-                            result = PuArrayList.fromJSON(responseText);
-                        } else if (responseText.startsWith("{")) {
-                            result = PuObject.fromJSON(responseText);
-                        } else if (responseText.startsWith("<")) {
-                            result = PuXmlHelper.parseXml(responseText);
-                        } else {
-                            result = new PuValue(responseText, PuDataType.STRING);
-                        }
-                    } catch (Exception ex) {
-                        result = new PuValue(responseText, PuDataType.STRING);
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Error while consuming response entity", e);
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public PuElement getPuElement() throws InterruptedException, ExecutionException {
-        return this.handleResponse(this.get());
-    }
+	@Override
+	public PuElement getPuElement() throws InterruptedException, ExecutionException {
+		return this.handleResponse(this.get());
+	}
 }
